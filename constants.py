7feecbc47a9ebf3edb4cdf68be4cc316b3171799
Debayan Pratihar <121422042+debayanpratihar@users.noreply.github.com>
2024-09-19 13:27:19 +0530

    second

--- constants.py
+++ constants.py
@@ -1,9 +1,11 @@
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
 SERVER_URL = 'localhost'
-PORT = '8900'
+# Use the PORT provided by the environment if available, otherwise default to '8900'
+PORT = os.getenv('PORT', '8900') 
 ENV = 'dev'
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
